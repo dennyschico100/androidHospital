f6fb20f3dc27b6bfe7f519de30b6567929e0c544
IsaiasOrtiz <58859695+IsaiasOrtiz@users.noreply.github.com>
2020-11-28 13:40:13 -0600

    Agregando 2 activitys programadas.

---
+++ app/src/main/java/com/example/hospital/PacientesDoctores.java
@@ -0,0 +1,122 @@
+package com.example.hospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+import datos.ConexionSqlLite;
+
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class PacientesDoctores extends AppCompatActivity {
+    private ArrayList<Paciente> listaPacientes;
+    private ListView lvPacientes;
+    private EditText edtBuscar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pacientes_doctores);
+        llenarLista();
+        Adaptador ad=new Adaptador(this);
+        lvPacientes=(ListView) findViewById(R.id.lvListaPacientes);
+        edtBuscar = (EditText)  findViewById(R.id.edtBuscar);
+        lvPacientes.setAdapter(ad);
+
+
+    }
+    class Adaptador extends ArrayAdapter<Paciente> {
+        AppCompatActivity appCompatActivity;
+
+        Adaptador(AppCompatActivity context){
+            super(context, R.layout.listas_personalizadas, listaPacientes);
+            appCompatActivity=context;
+        }
+        public View getView(int position, View convertView, ViewGroup parent){
+
+            LayoutInflater inflater=appCompatActivity.getLayoutInflater();
+            @SuppressLint("ViewHolder") View item=inflater.inflate(R.layout.listas_personalizadas , null);
+            TextView textView1 =item.findViewById(R.id.txtNombreLista);
+            TextView descripcion = item.findViewById(R.id.tvDescripcion);
+            ImageView imageView1 =item.findViewById(R.id.img);
+                textView1.setText(listaPacientes.get(position).getNombres() +" "+listaPacientes.get(position).getApellidos());
+                descripcion.setText("DUI:"+listaPacientes.get(position).getDuiPaciente()+"\nEdad:"
+                        +listaPacientes.get(position).getEdad()+"\nHabitacion:"+listaPacientes.get(position).getIdHabitacion());
+                imageView1.setImageResource(R.drawable.recursos20);
+
+            return (item);
+        }
+    }
+    public void llenarLista() {
+        listaPacientes = new ArrayList<Paciente>();
+        String sql="SELECT * FROM paciente";
+        Cursor datos = getConexion().rawQuery(sql, null);
+        while(datos.moveToNext())
+        {
+           addListItem(datos);
+        }
+    }
+    public boolean validar()
+    {
+        if(edtBuscar.getText().toString().isEmpty()){
+            edtBuscar.setError("Ingrese el valor a buscar");
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+    public void addListItem(Cursor datos)
+    {
+        listaPacientes.add(new Paciente(datos.getString(1),
+                datos.getString(2),
+                datos.getString(3),
+                datos.getInt(4),
+                datos.getInt(5),
+                datos.getString(6),
+                datos.getInt(7),
+                datos.getInt(8),
+                datos.getString(9),
+                datos.getString(10)));
+    }
+    public void buscar(View v){
+        if(validar()){
+            String sql="select * from paciente where nombres like '%"+edtBuscar.getText().toString()+"%'";
+
+            Cursor datos = getConexion().rawQuery(sql, null);
+            int cont=0;
+            if(datos.moveToNext())
+            {
+                listaPacientes=new ArrayList<>();
+                cont++;
+                addListItem(datos);
+                while (datos.moveToNext()){
+                    addListItem(datos);
+                    cont++;
+                }
+                Adaptador ad2=new Adaptador(this);
+                lvPacientes.setAdapter(ad2);
+            }else{
+                Toast.makeText(this, "No se encontraron registros", Toast.LENGTH_SHORT).show();
+            }
+            if(cont!=0)
+                Toast.makeText(this, "Se encontraron: "+cont+" Registros", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    private SQLiteDatabase getConexion()
+    {
+        ConexionSqlLite objConexion = new ConexionSqlLite(getApplicationContext());
+        return objConexion.getWritableDatabase();
+    }
+}
--- app/src/main/java/com/example/hospital/PaginaPrincipal_I.java
+++ app/src/main/java/com/example/hospital/PaginaPrincipal_I.java
@@ -1,20 +1,56 @@
 package com.example.hospital;
 
 import androidx.appcompat.app.AppCompatActivity;
+import datos.ConexionSqlLite;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class PaginaPrincipal_I extends AppCompatActivity {
-    private TextView fecha;
+    private TextView fecha,camasOcupadas, camasDisponibles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_principal__i);
         fecha = (TextView)  findViewById(R.id.tvFecha);
+        camasOcupadas= (TextView) findViewById(R.id.tvCamasDisponibles);
+        camasDisponibles=(TextView) findViewById(R.id.tvCamasOcupadas);
+
         Calendar calendar = Calendar.getInstance();
         fecha.setText(calendar.getTime().toString());
+        getDatosInicio();
+
+
+
     }
+
+    public void getDatosInicio()
+    {
+        String consulta = "select * from habitaciones";
+        Cursor datos = getConexion().rawQuery(consulta, null);
+        int camasDisponibles1=0;
+        int totalCamas=0;
+        int quirofanosDisponibles=0;
+        while(datos.moveToNext())
+        {
+           camasDisponibles1=camasDisponibles1+datos.getInt(3);
+           totalCamas = totalCamas + datos.getInt(2);
+           if(datos.getString(4).toUpperCase().equals("quirofano".toUpperCase()))
+           {
+               quirofanosDisponibles=quirofanosDisponibles+camasDisponibles1+datos.getInt(3);
+           }
+        }
+        camasDisponibles.setText(String.valueOf(camasDisponibles1));
+        camasOcupadas.setText(String.valueOf(totalCamas-camasDisponibles1));
+    }
+    private SQLiteDatabase getConexion()
+    {
+        ConexionSqlLite objConexion = new ConexionSqlLite(getApplicationContext());
+        return objConexion.getWritableDatabase();
+    }
+
 }
